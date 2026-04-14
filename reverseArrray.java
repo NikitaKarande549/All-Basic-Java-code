aa496bb8daf97cbd59eff6bc68a3e5b7bbf847ae
Nikita Karande <nikitaskarande@coep.sveri.ac.in>
2026-04-14 18:54:10 +0530

    reverse array

---
+++ reverseArrray.java
@@ -0,0 +1,12 @@
+public class reverseArrray {
+    public static void main(String[] args)
+    {
+        int arr[]={3,4,5,6};
+
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            System.out.print(" "+arr[i]);
+        }
+    } 
+    
+}
