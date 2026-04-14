8cb725bfc2170a0eab498f2567249dd611580b15
Nikita Karande <nikitaskarande@coep.sveri.ac.in>
2026-04-14 16:59:51 +0530

    sum of natural numbers

---
+++ sumOfNaturalNumbers.java
@@ -0,0 +1,11 @@
+public class sumOfNaturalNumbers {
+    public static void main(String[] args) {
+        int a=8;
+        int b=9;
+        int c=3;
+
+        int sum=a+b+c;
+
+        System.out.println("sum of natural numbers is: "+ sum);
+    }
+}
